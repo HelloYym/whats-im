fbe5d739c4349f81161a01ac02c8a091149bbab1
杨煜溟 <yymzjuer@outlook.com>
2016-04-15 13:49:55 +0800

    initial

---
+++ WhatsIM/urls.py
@@ -0,0 +1,28 @@
+"""WhatsIM URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from index import views as index_views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', index_views.index),
+    url(r'^login/', index_views.login),
+    url(r'^register/', index_views.register),
+    url(r'^logout/', index_views.logout),
+    url(r'^check_username/', index_views.check_username),
+    url(r'^check_passwd/', index_views.check_passwd),
+]
---
+++ index/views.py
@@ -0,0 +1,66 @@
+# coding:utf-8
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+
+def index(request):
+    # if request.user.is_authenticated():
+    #     return render(request, "home.html")
+    return render(request, "index.html")
+
+
+def login(request):
+    username = request.POST.get('username', None)
+    password = request.POST.get('password', None)
+    print username, password
+    user = auth.authenticate(username=username, password=password)
+    print user
+    if user is not None:
+        auth.login(request, user)
+        return render(request, "home.html")
+    else:
+        return HttpResponseRedirect("/")
+
+
+def register(request):
+    username = request.POST.get('username', None)
+    password = request.POST.get('password', None)
+    email = request.POST.get('email', None)
+    user = User.objects.filter(username=request.POST['username'])
+    print username, password, email
+    if user.count():
+        return HttpResponse('account has already exits.')
+    else:
+        user = User.objects.create_user(username=username, password=password, email=email)
+        user.save()
+        return render(request, "home.html")
+
+
+def check_username(request):
+    username = request.POST.get('username', None)
+    user = User.objects.filter(username=request.POST['username'])
+    print username
+    if user.count():
+        return HttpResponse("yes")
+    else:
+        return HttpResponse("no")
+
+
+def check_passwd(request):
+    username = request.POST.get('username', None)
+    password = request.POST.get('password', None)
+    print username, password
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        return HttpResponse("yes")
+    else:
+        return HttpResponse("no")
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request, 'index.html')
